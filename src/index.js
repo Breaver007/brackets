a6f6d9f9194d38cbf2535fd0c6f2db52bbfa63da
Breaver007 <96075427+Breaver007@users.noreply.github.com>
2022-02-20 22:24:24 +0300

    feat: task v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var arr = [];
+    for (let char of str) {
+        arr.push(char);
+    }
+    var result = {};
+    let sum = 0;
+    for (var i = 0; i < arr.length; ++i) {
+        var a = arr[i];
+        if (result[a] != undefined)
+            ++result[a];
+        else
+            result[a] = 1;
+    }
+    for (var key in result) {
+        sum += result[key];
+    }
+    if (sum % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
